7f57b3cab39ef8fdafb25a1ffc3180dfe8ca9324
silvyfebryana <silvydharmafebryana@gmail.com>
2020-04-24 11:57:02 +0700

    🤞 first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom'
 
+import './views/components/Button/Button.css'
 import './App.css';
 
 import ButtonUI from './views/components/Button/Button'
-import './views/components/Button/Button.css'
+
 
 
 class App extends React.Component {
